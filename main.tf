3ea190f9e3d05ba0b09843a7c9cfbd3d587023c5
Dennis Hoppe <github@dhoppe.it>
2021-12-22 00:02:18 +0100

    feat: Add support for BitBucket Server and Azure DevOps Server

--- main.tf
+++ main.tf
@@ -4,5 +4,8 @@ resource "tfe_oauth_client" "default" {
   http_url         = var.http_url
   oauth_token      = var.oauth_token
   private_key      = var.private_key
+  key              = var.key
+  secret           = var.secret
+  rsa_public_key   = var.rsa_public_key
   service_provider = var.service_provider
 }
