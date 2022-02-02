5c1dba3caa6771c514782c1badbee335a3c22a17
Dennis Hoppe <github@dhoppe.it>
2022-02-02 19:58:25 +0100

    feat: Add ID of the OAuth client as output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "id" {
+  value       = tfe_oauth_client.default.id
+  description = "The ID of the OAuth client"
+}
+
 output "oauth_token_id" {
   value       = tfe_oauth_client.default.oauth_token_id
   description = "The ID of the OAuth token associated with the OAuth client"
